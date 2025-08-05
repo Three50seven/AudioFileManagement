3c87299d2c528edefbdb959de0364c373c8771d8
Paul Krimm <pm.krimm@gmail.com>
2025-08-05 11:25:11 -0400

    Add project files.

---
+++ MP3FileManager/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("MP3 File Manager - manages metatags and updates the specified library with matching (hopefully) higher-quality files, if specified.");
